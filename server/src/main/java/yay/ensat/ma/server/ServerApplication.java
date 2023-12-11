2a150089755ac6bcac44cded14737fa4d30969b8
youssefharrak1 <133062339+youssefharrak1@users.noreply.github.com>
2023-12-11 19:45:20 +0100

    test

--- server/src/main/java/yay/ensat/ma/server/ServerApplication.java
+++ server/src/main/java/yay/ensat/ma/server/ServerApplication.java
@@ -1,7 +1,9 @@
 package yay.ensat.ma.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +12,10 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(){
+        return args -> {
+            System.out.println("yay project");
+        };
+    }
 }
