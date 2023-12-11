6d0fb209e7caff5a65856ecd1584b96954e552b2
YassineIDR <yassineidrissi2005@gmail.com>
2023-12-11 12:50:28 +0100

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import LoginForm from "./components/LoginForm"
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import RootLayout from "./layouts/RootLayout"
+import PageNotFound from "./layouts/PageNotFound"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<LoginForm/>}/>
+
+      <Route path='*' element={<PageNotFound/>}></Route>
+    </Route>
+  )
+)
 
 function App() {
   
 
   return (
     <>
-      <LoginForm/>
+      <RouterProvider router={router}/>
     </>
   )
 }
