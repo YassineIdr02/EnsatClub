ec96d12f9987343bc87fa8bcafa609056cba0081
youssefharrak1 <133062339+youssefharrak1@users.noreply.github.com>
2023-12-13 07:52:22 +0100

    added dtos

--- server/src/main/java/yay/ensat/ma/server/dtos/GalleryDTO.java
+++ server/src/main/java/yay/ensat/ma/server/dtos/GalleryDTO.java
@@ -4,7 +4,7 @@ package yay.ensat.ma.server.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import yay.ensat.ma.ensatclub.entities.Activity;
+
 
 import java.util.List;
 @Data
@@ -16,5 +16,5 @@ public class GalleryDTO {
     // we can do also ClubDTO but yassine want only the club_id,
     // so we need to create a mapper for that !!!!!!
     private Long clubId;
-    private List<Activity> activities;
+    private List<ActivityDTO> activities;
 }
