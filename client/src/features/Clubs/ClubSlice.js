5b69da5cc4a5bf87a5bff8eb37cfa0bfa42c0d77
YassineIDR <yassineidrissi2005@gmail.com>
2023-12-29 12:15:19 +0100

    Members

--- client/src/features/Clubs/ClubSlice.js
+++ client/src/features/Clubs/ClubSlice.js
@@ -7,7 +7,7 @@ const config = {
   headers: {
     Authorization: `Bearer ${Cookies.get("token")}`,
   },
-}
+};
 
 const BASE_URL = "http://localhost:8080";
 
@@ -16,11 +16,11 @@ export const addClub = createAsyncThunk("newclub/addClub", async (payload) => {
     const formData = new FormData();
     formData.append("name", payload.clubName);
     formData.append("description", payload.clubDescription);
-    const response = await axios.post(`${BASE_URL}/newclub`,formData,config);
+    const response = await axios.post(`${BASE_URL}/newclub`, formData, config);
     return response.data;
   } catch (err) {
     console.log(err);
-    throw err; 
+    throw err;
   }
 });
 
@@ -30,7 +30,7 @@ export const getClubs = createAsyncThunk("allclubs/getclubs", async () => {
     return response.data;
   } catch (error) {
     console.log(error);
-    throw error; 
+    throw error;
   }
 });
 
@@ -54,12 +54,36 @@ export const assocPresident = createAsyncThunk(
   }
 );
 
+export const newMember = createAsyncThunk(
+  "newmember/newMember",
+  async (payload) => {
+    try {
+      const { clubId } = payload;
+      const formData = new FormData();
+      formData.append("club_id", clubId);
+      formData.append("role", "member");
+      formData.append("name", payload.firstName + " " + payload.lastName);
+      const response = await axios.post(
+        `${BASE_URL}/newmember`,
+        formData,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 export const getMembers = createAsyncThunk(
   "clubmembers/getMembers",
   async (payload) => {
     const { clubId } = payload;
     try {
-      const response = await axios.get(`${BASE_URL}/clubmembers/${clubId}`,config);
+      const response = await axios.get(
+        `${BASE_URL}/clubmembers/${clubId}`,
+        config
+      );
       return response.data;
     } catch (error) {
       console.log(error);
@@ -81,20 +105,21 @@ export const ClubSlice = createSlice({
     },
   },
   extraReducers(builder) {
-    builder.addCase(getClubs.fulfilled, (state, action) => {
-      state.clubStatus = "success";
-      state.clubs = [...action.payload];
-    })
-    .addCase(getClubs.pending, (state, action) => {
-      state.clubStatus = "loading";
-    })
-    .addCase(getMembers.pending, (state, action) => {
-      state.memberStatus = "loading";
-    } )
-    .addCase(getMembers.fulfilled, (state, action)=> {
-      state.memberStatus = "success";
-      state.members = [...action.payload];
-    })
+    builder
+      .addCase(getClubs.fulfilled, (state, action) => {
+        state.clubStatus = "success";
+        state.clubs = [...action.payload];
+      })
+      .addCase(getClubs.pending, (state, action) => {
+        state.clubStatus = "loading";
+      })
+      .addCase(getMembers.pending, (state, action) => {
+        state.memberStatus = "loading";
+      })
+      .addCase(getMembers.fulfilled, (state, action) => {
+        state.memberStatus = "success";
+        state.members = [...action.payload];
+      });
   },
 });
 
@@ -105,6 +130,7 @@ export const getAllMembers = (state) => state.club.members;
 
 export const getClubStatus = (state) => state.club.clubStatus;
 export const getMemberStatus = (state) => state.club.memberStatus;
+export const getClubById = (state, clubId) => state.club.clubs.find((club)=> club.id == clubId)
 
 export const getPresidentByClubId = (state, clubId) => {
   return state.club.clubs.find((c) => c.id == clubId);
